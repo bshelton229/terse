50b391ae57dfed38b64b08e27cd603f4cb1d3f47
Bryan Shelton <bryan@sheltonplace.com>
2013-11-21 17:16:58 -0800

    URL validation.

--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe Url do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  it 'Should only allow valid URLS' do
+    %w{
+      http:
+      nothing
+      bryan.com/hello
+      http://
+    }.each do |url|
+      url = Url.new full: url
+      expect(url).to_not be_valid
+    end
+  end
+
+  it 'Should allow good urls' do
+    %w{
+      http://test.com
+      http://sheltonplace.com/test?url=string&another=string
+      https://domain.com
+      https://domain.com/path
+    }.each do |url|
+      url = Url.new full: url
+      expect(url).to be_valid
+    end
+  end
 end
