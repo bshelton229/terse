aadc4bb26a39bb27fa4b740d9b8ec2cf2f17979c
Bryan Shelton <bryan@sheltonplace.com>
2013-11-21 16:50:04 -0800

    Add a non-production endpoint for omniauth testing without a Github app.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,15 @@
 class SessionsController < ApplicationController
+  skip_before_filter :verify_authenticity_token, only: :developer_strategy
+
   def create
     if user = User.from_github_auth(auth)
       session[:user_id] = user.id
     end
     redirect_to root_path
   end
+
+  # Developer endpoint
+  def developer_strategy;create;end
 
   def destroy
     session[:user_id] = nil
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Terse::Application.routes.draw do
   root 'urls#index'
   resources :urls
   get '/auth/github/callback', to: 'sessions#create'
+
+  # Development omniauth-github
+  post '/auth/developer/callback', to: 'sessions#developer_strategy' unless Rails.env.production?
+
   get '/logout', to: 'sessions#destroy', as: 'logout'
 end
