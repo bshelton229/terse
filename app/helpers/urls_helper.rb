7fb1d8698608b5bf489874b4bc642575725b1e86
Bryan Shelton <bryan@sheltonplace.com>
2013-11-22 09:55:34 -0800

    Url widget display work.

--- app/helpers/urls_helper.rb
+++ app/helpers/urls_helper.rb
@@ -3,4 +3,10 @@ module UrlsHelper
   def short_url(url)
     resolve_url(slug: url.slug)
   end
+
+  def url_display_name(url)
+    out = [link_to(url.full, url.full)]
+    out << "(#{url.title})" if url.title.present?
+    raw out.join ' '
+  end
 end
